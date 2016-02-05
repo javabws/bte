0cc0537d471b916ad8842a44d0717271c5639a78
javabws <javabwssangeeth@gmail.com>
2016-02-05 13:04:51 +0530

    Changed message content and sendby email

--- bte/src/com/btx/Process/EmailProcess.java
+++ bte/src/com/btx/Process/EmailProcess.java
@@ -10,7 +10,7 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 public class EmailProcess {
 	
 	//----Method is user to send verification code to newly registered users
-	public static ClientResponse SendVerificationCode(String vco, String email,
+	synchronized public static ClientResponse SendVerificationCode(String vco, String email,
 			String path) {
 		Client client = Client.create();
 		client.addFilter(new HTTPBasicAuthFilter("api",
@@ -19,19 +19,45 @@ public class EmailProcess {
 				.resource("https://api.mailgun.net/v3/sandboxbdc50af29fe346df9c9983e8119db61f.mailgun.org/messages");
 
 		MultivaluedMapImpl formData = new MultivaluedMapImpl();
-		formData.add("from", "Support team <support@btx.com>");
+		formData.add("from", "Support Team <support@binarytradeexchange.com>");
 		formData.add("to", email);
-		formData.add("subject", "Btx verification");
-		formData.add("html", "Click this link for verification <br><a href=\""
-				+ path + "?acode="+ vco
-				+ "\" >Click to Activate your account</a>");
+		formData.add("subject", "BinaryTradeExchange - Account Activation - Email Verification");
+		formData.add("html", "<h2>Congratulations on joining Binary Trade Exchange</h2><p>We received your registration to join Binary Trade Exchange-A "
+				+ "Revolution of Binary Option. Welcome to the launch of the most "
+				+ "innovative Binary Option platform. It is the first of its kind in the "
+				+ "world, a combination of Binary Option market and the exchange "
+				+ "platform.</p>"
+				+ "<p>As a token of appreciation, we are giving away 100 proven Binary "
+				+ "Option strategies with some of it giving an amazing 95% strike rate "
+				+ "totally free with no strings attached.</p>"
+				+ "<p>We have many reasons why you should choose us comparing to "
+				+ "traditional Binary Option brokers:</p>"
+				+ "<ul>"
+				+ "<li><b>90% maximum return in 60 seconds, highest payout in this industry.</b></li>"
+				+ "<li><b>Trade till the final second.</b></li>"
+				+ "<li><b>Peer to peer trading, Not against the brokers.</b></li>"
+				+ "<li><b>We earn commission only when you win.</b></li>"
+				+ "<li><b>48 hours withdrawal.</b></li>"
+				+ "<li><b>100% encrypted data transfer and highest account security.</b></li>"
+				+ "<li><b>Professional customer support available via live chat.</b></li>"
+				+ "</ul>"
+				+ "<p>We're excited to see what results we can create together with "
+				+ "you in Binary Trade Exchange</p>"
+				+ "<p>Please click the link below to activate your account:</p>"
+				+ "<p>Click this link for verification</p>"
+				+ "<p><a href=\""+ path + "?acode="+ vco+ "\" >Click to Activate your account</a></p>"
+				+ "<p>With much appreciation,</p>"
+				+ "<p>"
+				+ "<h4>Mather Ray Vran</h4>"
+				+ "</p>"
+				+ "<p><h4>BTX Admin Head</h4></p>");
 		return (ClientResponse) webResource.type(
 				"application/x-www-form-urlencoded").post(ClientResponse.class,
 				formData);
 	}
 
 	//-----Method is used to send the referral link
-	public static ClientResponse SendReferralLink(String rcode, String email,
+	synchronized public static ClientResponse SendReferralLink(String rcode, String email,
 			String path) {
 		Client client = Client.create();
 		client.addFilter(new HTTPBasicAuthFilter("api",
@@ -40,9 +66,9 @@ public class EmailProcess {
 				.resource("https://api.mailgun.net/v3/sandboxbdc50af29fe346df9c9983e8119db61f.mailgun.org/messages");
 
 		MultivaluedMapImpl formData = new MultivaluedMapImpl();
-		formData.add("from", "Support team <support@btx.com>");
+		formData.add("from", "Support team <support@binarytradeexchange.com>");
 		formData.add("to", email);
-		formData.add("subject", "Btx Referral Link");
+		formData.add("subject", "BinaryTradeExchange - Referral");
 		formData.add("html", "Click this link for Registration <br><a href=\""
 				+ path + "?r="+ rcode
 				+ "\" >Click to Register a new account</a>");
@@ -52,7 +78,7 @@ public class EmailProcess {
 	}
 	
 	//---This method is used to send the forget password link to users
-	public static ClientResponse SendForgetPasswordLink(String rcode, String email,
+	synchronized public static ClientResponse SendForgetPasswordLink(String rcode, String email,
 			String path) {
 		Client client = Client.create();
 		client.addFilter(new HTTPBasicAuthFilter("api",
@@ -61,9 +87,9 @@ public class EmailProcess {
 				.resource("https://api.mailgun.net/v3/sandboxbdc50af29fe346df9c9983e8119db61f.mailgun.org/messages");
 
 		MultivaluedMapImpl formData = new MultivaluedMapImpl();
-		formData.add("from", "Support team <support@btx.com>");
+		formData.add("from", "Support team <support@binarytradeexchange.com>");
 		formData.add("to", email);
-		formData.add("subject", "Btx Forget Password Link");
+		formData.add("subject", "BinaryTradeExchange - Forget Password");
 		formData.add("html", "Click this link for Reset Password <br><a href=\""
 				+ path + "?r="+ rcode
 				+ "\" >Click to Reset Password</a>");
@@ -74,7 +100,7 @@ public class EmailProcess {
 	
 	
 	//----This method id used to send the mail for Contact Support page in Landing page
-	public static ClientResponse SendContactSupportMail(ContactHomeBean c,String email) {
+	synchronized public static ClientResponse SendContactSupportMail(ContactHomeBean c,String email) {
 		Client client = Client.create();
 		client.addFilter(new HTTPBasicAuthFilter("api",
 				"key-de7b4217e230468a641b7e6ecd1bce5b"));
@@ -82,7 +108,7 @@ public class EmailProcess {
 				.resource("https://api.mailgun.net/v3/sandboxbdc50af29fe346df9c9983e8119db61f.mailgun.org/messages");
 
 		MultivaluedMapImpl formData = new MultivaluedMapImpl();
-		formData.add("from", "General Contact Support Mail <support@btx.com>");
+		formData.add("from", "General Contact Support Mail <support@binarytradeexchange.com>");
 		formData.add("to", email);
 		formData.add("subject", "Contact Support");
 		formData.add("html", "Title: "+c.getTitle()+"<br>"
